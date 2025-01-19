615678aca2a7ce89ce3301f41c2aaa2272ea9da2
Pili <pilarriusm@gmail.com>
2025-01-19 14:30:50 +0100

    'bug fix for deployment'

--- eslint.config.js
+++ eslint.config.js
@@ -4,25 +4,13 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+export default {
+  languageOptions: {
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'], // Add your tsconfig paths
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-)
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommendedTypeChecked'],
+};
+
